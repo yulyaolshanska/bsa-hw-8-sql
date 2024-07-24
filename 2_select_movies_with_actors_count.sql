9605f5c5a88382ffceb769f289e463083d7f7a46
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-07-24 21:05:35 +0300

    fix: (2 select) add actors, who dont have characte

--- 2_select_movies_with_actors_count.sql
+++ 2_select_movies_with_actors_count.sql
@@ -1,12 +1,22 @@
 SELECT
     m.movie_id AS ID,
     m.title,
-    COUNT(mc.character_id) AS actors_count
+    COUNT(DISTINCT all_actors.actor_id) AS actors_count
 FROM
     Movies m
-JOIN
-    MovieCharacters mc ON m.movie_id = mc.movie_id
+LEFT JOIN
+    (
+        SELECT a.person_id AS actor_id, mc.movie_id
+        FROM MovieCharacters mc
+        JOIN Actors a ON mc.character_id = a.character_id
+        UNION
+        SELECT ma.person_id AS actor_id, ma.movie_id
+        FROM MovieActors ma
+    ) AS all_actors
+ON m.movie_id = all_actors.movie_id
 WHERE
     m.release_date >= NOW() - INTERVAL '5 years'
 GROUP BY
-    m.movie_id, m.title;
+    m.movie_id, m.title
+ORDER BY
+    m.title;
