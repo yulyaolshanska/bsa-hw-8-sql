e7d7bd1c00d4271597c43e2eba9b4b1fa5ff2d4c
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-07-24 21:02:32 +0300

    fix: (1.select) add Actors who dont have role

--- 1_select_actors_with_total_movies_budget.sql
+++ 1_select_actors_with_total_movies_budget.sql
@@ -2,14 +2,14 @@ SELECT
     p.person_id AS ID,
     p.first_name,
     p.last_name,
-    SUM(m.budget) AS total_movies_budget
+    COALESCE(SUM(m.budget), 0) AS total_movies_budget
 FROM
     Persons p
-JOIN
+LEFT JOIN
     Actors a ON p.person_id = a.person_id
-JOIN
+LEFT JOIN
     MovieCharacters mc ON a.character_id = mc.character_id
-JOIN
+LEFT JOIN
     Movies m ON mc.movie_id = m.movie_id
 GROUP BY
     p.person_id, p.first_name, p.last_name;
