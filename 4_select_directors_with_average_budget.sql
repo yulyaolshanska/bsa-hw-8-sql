6636ee98f88fa656058e5aacd31e762ea57c8fa7
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-07-26 13:56:57 +0300

    refactor: total format

--- 4_select_directors_with_average_budget.sql
+++ 4_select_directors_with_average_budget.sql
@@ -1,7 +1,7 @@
 SELECT
     p.person_id AS director_id,
     CONCAT(p.first_name, ' ', p.last_name) AS director_name,
-    AVG(m.budget) AS average_budget
+    FLOOR(AVG(m.budget)) AS average_budget
 FROM
     Persons p
 JOIN
